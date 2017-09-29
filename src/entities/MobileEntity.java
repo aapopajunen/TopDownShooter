76b11d5ab0c3a0ff5557ffc2b4db9f36ef4721a7
Aapo Pajunen <aapo.pajunen@gmail.com>
2017-09-29 16:29:28 +0300

    Added HP to mobileEnemy.

--- src/entities/MobileEntity.java
+++ src/entities/MobileEntity.java
@@ -2,10 +2,12 @@ package entities;
 
 public abstract class MobileEntity extends Entity{
 
+
+    public double hp = 1.0;
+
     public MobileEntity(double x, double y, double width, double height) {
         super(x, y, width, height);
     }
-
 
 
 }
