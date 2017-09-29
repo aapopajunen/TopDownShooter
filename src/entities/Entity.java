f39abc646ebdc1d37004eaa4ac504a3d639b28df
Otto <otto.vonboehm@gmail.com>
2017-09-29 17:39:54 +0300

    Added getters to Entity class

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,7 +1,5 @@
 package entities;
 
-import javafx.scene.shape.Polygon;
-
 import java.awt.geom.Rectangle2D;
 
 public abstract class Entity {
@@ -9,7 +7,20 @@ public abstract class Entity {
     protected Rectangle2D hitbox;
 
     public Entity(double x,double y,double width, double height) {
-        hitbox.setRect(x, y, width, height);
+        hitbox = new Rectangle2D.Double(x, y, width, height);
+    }
+
+    public double getX() {
+        return this.hitbox.getX();
+    }
+    public double getY() {
+        return this.hitbox.getY();
+    }
+    public double getWidth() {
+        return this.hitbox.getWidth();
+    }
+    public double getHeight() {
+        return this.hitbox.getHeight();
     }
 
     public abstract void update(double dt);
